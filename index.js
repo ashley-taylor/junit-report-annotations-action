95c8a32ac071ac71103b110a553c5784c834dbb4
Ashley <Ashley@host.docker.internal>
2020-03-03 22:04:13 +1300

    adding libraries

---
+++ index.js
@@ -0,0 +1,19 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const glob = require('@actions/glob');
+const parser = require('xml2json');
+try {
+  const path = core.getInput('path');
+  const includeSummary = core.getInput('includeSummary');
+  const numFailures = core.getInput('numFailures');
+
+  const globber = await glob.create(path, {followSymbolicLinks: false})
+
+  for await (const file of globber.globGenerator()) {
+    const data = await fs.readFile(file)
+    var json = parser.toJson(data);
+    console.log("to json ->", json);
+  }
+} catch (error) {
+  core.setFailed(error.message);
+}
