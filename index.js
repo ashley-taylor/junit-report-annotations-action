0bfce4334fcb845a0a7c297461defeec14eaaa51
Ashley <Ashley@host.docker.internal>
2020-03-03 22:29:45 +1300

    fix import

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const glob = require('@actions/glob');
 const parser = require('xml2json');
-const octokit = require('@octokit/rest')
+const { Octokit } = require('@octokit/rest');
 
 
 (async () => {
@@ -11,14 +11,14 @@ const octokit = require('@octokit/rest')
     const includeSummary = core.getInput('includeSummary');
     const numFailures = core.getInput('numFailures');
 
-    const globber = await glob.create(path, {followSymbolicLinks: false})
+    const globber = await glob.create(path, {followSymbolicLinks: false});
 
     for await (const file of globber.globGenerator()) {
-        const data = await fs.readFile(file)
+        const data = await fs.readFile(file);
         var json = parser.toJson(data);
         console.log("to json ->", json);
     }
     } catch (error) {
-    core.setFailed(error.message);
+   		core.setFailed(error.message);
     }
 })();
